612f27f59750c4456dacf986775430b61fb0e10f
Jeff T <tarutmauktoe@gmail.com>
2017-12-09 16:47:34 -0600

    Display error message when submitting new job and name is not filled in

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 //import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,14 +40,12 @@ public class JobController {
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     //public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
         if (errors.hasErrors()) {
-            model.addAttribute(new JobForm());
-            model.addAttribute(errors);
             return "new-job";
         }
         else {
